5ae002e2e2dc52bbd1b932398046d6f3176b9749
YashasKotian <22c62.yashas@sjec.ac.in>
2026-04-17 11:46:52 +0530

    Fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import CreatePost from './pages/CreatePost'
@@ -16,6 +16,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to='/allposts' replace />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/create-post' element={<CreatePost />} />
